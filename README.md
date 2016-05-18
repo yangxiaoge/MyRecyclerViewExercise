775b9d711499cc6833e73204ceee20dd40d5e102
yangxiaoge <364804051@qq.com>
2016-05-18 11:02:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# MyRecyclerView练习
+
+> 基于鸿神博客写了一遍
+
+[博客原文](http://blog.csdn.net/lmj623565791/article/details/45059587)
+
+## 1. 主要体验下 RecyclerView
+## 2. 对比ListView
---
+++ app/src/main/java/com/yjn/myrecyclerviewexercise/MainActivity.java
@@ -0,0 +1,73 @@
+package com.yjn.myrecyclerviewexercise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+     RecyclerView recyclerView;
+     List<String> mDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initDatas();
+        recyclerView = (RecyclerView) findViewById(R.id.id_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //设置分割线, Recycler不提供分割线,当然 我们可以自由的去定制它
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+
+        recyclerView.setAdapter(new HomeAdapter());
+
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    MainActivity.this).inflate(R.layout.item, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView tv;
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                tv = (TextView) itemView.findViewById(R.id.id_num);
+            }
+        }
+
+    }
+}
